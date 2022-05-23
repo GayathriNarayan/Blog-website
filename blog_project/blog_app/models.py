ceed2a64b50162110bda7fe10272170ed66d6aee
sariamancharla <93614345+sariamancharla@users.noreply.github.com>
2022-05-23 17:02:36 +0200

    Created UserProfileInfo and BlogPost model
    Included in models.py,admin.py. Included the makemigrations file for the models created.

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from django.urls import reverse
 
-# Create your models here.
+#UserProfileInfo table links to User table
+class UserProfileInfo(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    portfolio_site=models.URLField(blank=True)
+    profile_pic=models.ImageField(upload_to='profile_pics',default='default_profile.jpg')
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+#BlogPost table links to User table
+class BlogPost(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=255)
+    dateTime = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'blog_pics', default='default_blog.jpg')
+    
+    def __str__(self):
+        return str(self.author) + " Blog Title: " + self.title
+    
+    def portfolio_site(self):
+        return reverse('blogs')
+ 
+
+
