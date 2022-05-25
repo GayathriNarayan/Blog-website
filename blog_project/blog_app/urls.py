232687c08c6adec801ac1d1be917035b72efc226
Michelinealsabea <40604274+Michelinealsabea@users.noreply.github.com>
2022-05-25 10:15:16 +0200

    changing big letters to small letters

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     url(r'^login/$', views.user_login, name="user_login"),
     url(r'^logout/$', views.user_logout,name='logout'),
     #### BLOG PART ##################
-    url(r'^create/$', views.Create_Blog , name='Create_Blog'),
-    url(r'^delete_Blog/<id>/$', views.delete_Blog ,name='delete_Blog'),
-    url(r'^update_Blog/<id>/$', views.update_Blog, name='update_Blog'),
+    url(r'^create/$', views.Create_Blog , name='create_blog'),
+    url(r'^delete_blog/<id>/$', views.delete_blog ,name='delete_blog'),
+    url(r'^update_blog/<id>/$', views.update_blog, name='update_blog'),
 ]
 ]
