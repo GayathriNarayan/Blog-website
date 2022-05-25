336e903c873547a6fdd1e89687276445af289131
Michelinealsabea <40604274+Michelinealsabea@users.noreply.github.com>
2022-05-25 10:10:58 +0200

    adding forms.py

---
+++ blog_project/blog_app/forms.py
@@ -0,0 +1,23 @@
+
+from distutils.command.upload import upload
+from django import forms
+from mini_blog.models import BlogPost
+#, UserProfileInfo
+
+class New_Blog_Form(forms.ModelForm):
+     class Meta:
+        model = BlogPost
+        fields = ['user', 'title', 'content', 'dateTime', 'image']
+
+class View_Blog_Form(forms.ModelForm):
+     class Meta:
+        model = BlogPost
+        fields = ['user', 'title', 'content', 'dateTime', 'image']
+
+#class New_Comment_Form(forms.Form):
+ #   user_name = forms.CharField(max_length=20)
+  #  content=forms.CharField(widget=forms.Textarea)
+   # dateTime = forms.DateTimeField(auto_now_add=True)
+
+    #def __str__(self):
+     #   return self.title + ""+ self.dateTime + self.content
