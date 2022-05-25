914fc4710eb8bf9847ac49febd73d50253c47d0c
Michelinealsabea <40604274+Michelinealsabea@users.noreply.github.com>
2022-05-25 11:46:27 +0200

    change dateTime is readonly in forms.py

--- blog_project/blog_app/forms.py
+++ blog_project/blog_app/forms.py
@@ -13,7 +13,15 @@ class View_Blog_Form(forms.ModelForm):
      class Meta:
         model = BlogPost
         fields = ['user', 'title', 'content', 'dateTime', 'image']
+          
+          
+       # make dateTime is readonly
+     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["dateTime"].widget.attrs["readonly"] = True
 
+
+          
 #class New_Comment_Form(forms.Form):
  #   user_name = forms.CharField(max_length=20)
   #  content=forms.CharField(widget=forms.Textarea)
