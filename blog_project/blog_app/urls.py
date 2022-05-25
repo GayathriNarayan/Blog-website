e92e4a4a25a699e148ae49195db8c4b966157c8a
Michelinealsabea <40604274+Michelinealsabea@users.noreply.github.com>
2022-05-25 10:08:57 +0200

    update urls.py - add create, update, delete path

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -6,4 +6,9 @@ app_name='blog_app'
 urlpatterns = [
     url(r'^login/$', views.user_login, name="user_login"),
     url(r'^logout/$', views.user_logout,name='logout'),
+    #### BLOG PART ##################
+    url(r'^create/$', views.Create_Blog , name='Create_Blog'),
+    url(r'^delete_Blog/<id>/$', views.delete_Blog ,name='delete_Blog'),
+    url(r'^update_Blog/<id>/$', views.update_Blog, name='update_Blog'),
+]
 ]
