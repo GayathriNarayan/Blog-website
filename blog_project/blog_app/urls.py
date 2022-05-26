01ef3291b374264a4536de8287194b85daba0615
kanikakalra86 <84987241+kanikakalra86@users.noreply.github.com>
2022-05-26 09:28:13 +0200

    Added Blog List Display and Blog Detail View on read more click button

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from blog_app import views
+from blog_app.views import  BlogListView, BlogDetailView
+
+
+app_name = 'blog_app'
 
 urlpatterns = [
-    url(r'user_login', views.index, name="user_login"),
+    url(r'^blog_list/$', BlogListView.as_view(), name="blog_list"),
+    url(r'^blog_detail/(?P<pk>\d+)/$', BlogDetailView.as_view(), name="blog_detail"),
+    
 ]
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,7 +1,30 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import BlogPost
 
 # Create your views here.
 
 def index(request):
   return render(request,'blog_app/index.html')
+
+"""
+Class to use inbuilt ListView object to dipslay list  of all Blogs 
+
+"""
+
+class BlogListView(ListView):
+  model = BlogPost
+  context_object_name = 'posts'
+  template_name = "blog_app/blog_list.html"
+
+"""
+Class to use inbuilt DetailView object to dipslay detail of particular Blog
+
+"""
+
+class BlogDetailView(DetailView):
+  model = BlogPost
+  context_object_name = 'post'
+  template_name = "blog_app/blog_detail.html"
+
